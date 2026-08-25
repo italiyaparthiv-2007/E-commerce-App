709ac298251fd39f4947566939655b02cb105aff
Parthiv Italiya <italiyaparthiv@gmail.com>
2026-08-25 21:48:10 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+
+import dns from "dns";
+import orderRouter from "./routes/orderRoute.js";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+//App Config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//API endpoints
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter)
+
+
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+app.listen(port, () => {
+  console.log("Server is running on PORT : " + port);
+});
