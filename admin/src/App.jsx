67d65be3c489378ecb92feba00b964b563cab084
Parthiv Italiya <italiyaparthiv@gmail.com>
2026-08-25 22:25:40 +0530

    fix: standardize currency export format

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
-export const currency = '$'
+export const currency = "$";
 
 const App = () => {
   const [token, setToken] = useState(
