8c963129623f74a9e3e9ef17de8f67e7c96ae4b9
Parthiv Italiya <italiyaparthiv@gmail.com>
2026-08-25 22:14:57 +0530

    fix: correct import path for Orders component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import PlaceOrder from "./pages/PlaceOrder";
-import Orders from "./pages/orders";
+import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
